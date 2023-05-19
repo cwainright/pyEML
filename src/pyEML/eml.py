9162508f038f5415f65795ea064efb9ff533d583
cwainright-nps <charles_wainright@nps.gov>
2023-05-19 16:25:38 -0400

    abstracted `get()` and `delete()`

--- src/pyEML/eml.py
+++ src/pyEML/eml.py
@@ -50,8 +50,7 @@ class Eml():
             print(f'{bcolors.FAIL + bcolors.BOLD + bcolors.UNDERLINE}Process execution failed.\n{bcolors.ENDC}\n{bcolors.BOLD}`Eml`{bcolors.ENDC} not created.')
 
     def get_title(self):
-        """Get the dataset's title node(s)
-        """
+        """Get the dataset's title node(s)"""
 
         path = LOOKUPS['title']['path']
         target = LOOKUPS['title']['target']
@@ -67,48 +66,27 @@ class Eml():
             if node: # only returns an object if pretty == False
                 return node
 
-        # nodes = self.eml.find_all_nodes_by_path(path=path)
-        # target_ids = []
-        # for target in nodes:
-        #     target_ids.append(target.id)
-        # target_nodes = []
-        # for target in target_ids:
-        #     target_nodes.append(self.eml.get_node_instance(target))
-        # for target in target_nodes:
-        #     self.show_overview(target)
-
     def set_title(self, title:str):
+        """Set the dataset's title node
 
+        Args:
+            title (str): Value to assign as the metadata title.
+        """
         try:
             path = LOOKUPS['title']['path']
             parent = LOOKUPS['title']['parent']
-            node_target = LOOKUPS['title']['node_target']
+            target = LOOKUPS['title']['target']
 
-            assert title not in ('', None), f'{bcolors.FAIL + bcolors.BOLD + bcolors.UNDERLINE}Process execution failed.\n{bcolors.ENDC}You provided "{title}". `{node_target}` cannot be blank.'
-            assert len(title) >= 3, f'{bcolors.FAIL + bcolors.BOLD + bcolors.UNDERLINE}Process execution failed.\n{bcolors.ENDC}You provided "{title}". {bcolors.BOLD}`{node_target}`{bcolors.ENDC} must be at least three characters.'
+            assert title not in ('', None), f'{bcolors.FAIL + bcolors.BOLD + bcolors.UNDERLINE}Process execution failed.\n{bcolors.ENDC}You provided "{title}". `{target}` cannot be blank.'
+            assert len(title) >= 3, f'{bcolors.FAIL + bcolors.BOLD + bcolors.UNDERLINE}Process execution failed.\n{bcolors.ENDC}You provided "{title}". {bcolors.BOLD}`{target}`{bcolors.ENDC} must be at least three characters.'
 
-            nodes = self.eml.find_all_nodes_by_path(path=path)
-            parents = self.eml.find_all_nodes_by_path(path=parent)
-            assert len(parents) == 1, print(f'{bcolors.FAIL + bcolors.BOLD + bcolors.UNDERLINE}Process execution failed.\n{bcolors.ENDC}\nReturned multiple parent{bcolors.BOLD}`node`{bcolors.ENDC}s:\n')
-            parent = parents[0]
+            self._set_node()
 
-            new_node = Node(path[-1])
-            new_node.content = title
-
-            if len(nodes) == 0:
-                parent.add_child(new_node)
-            else:
-                self.delete_title()
-                parent.add_child(new_node)
-            if self.interactive == True:
-                print('success')
-                self.get_title()
         except AssertionError as a:
             print(a)
 
-
     def delete_title(self):
-        """Delete the dataset's title node(s)
+        """Delete the dataset's title node
 
         Args:
             None
@@ -119,19 +97,65 @@ class Eml():
         try:
             path = LOOKUPS['title']['path']
             parent = LOOKUPS['title']['parent']
+            target = LOOKUPS['title']['target']
 
-            values = self.eml.find_all_nodes_by_path(path=path)
-            parent = self.eml.find_all_nodes_by_path(path=parent)
-            assert len(parent) == 1, print(f'{bcolors.FAIL + bcolors.BOLD + bcolors.UNDERLINE}Process execution failed.\n{bcolors.ENDC}\nReturned multiple parent{bcolors.BOLD}`node`{bcolors.ENDC}s:\n')
-            parent = parent[0]
-            for value in values:
-                parent.remove_child(child=value)
-                self.eml.delete_node_instance(id=value.id)
+            if self.interactive == True:
+                quiet = False
+            else:
+                quiet = True
 
-            # if self.interactive == True:
-            #     self.show_overview()
-        except AssertionError as a:
-            print(a)
+            self._delete_node(path=path, parent=parent, target=target, quiet=quiet)
+            
+        except:
+            print('error delete_title()') 
+
+    def get_creator(self):
+        path = LOOKUPS['creator']['path']
+        target = LOOKUPS['creator']['target']
+
+        if self.interactive == True:
+            pretty=True
+            quiet=False
+            self._get_node(path=path, target=target, pretty=pretty, quiet=quiet)
+        else:
+            pretty=False
+            quiet=True
+            node = self._get_node(path=path, target=target, pretty=pretty, quiet=quiet)
+            if node: # only returns an object if pretty == False
+                return node
+
+    def set_creator(self, creators:dict, append:bool=False):
+        """Delete the dataset's creator node(s)
+
+        Args:
+            creators (dict): A dictionary of creator names and contact information.
+            append (bool, optional): True will append `creators` dictionary to existing creators records. False will replace existing creators records with `creators` dict. Defaults to False.
+        """
+        pass
+
+    def delete_creator(self):
+        """Delete the dataset's creator node(s)
+
+        Args:
+            None
+
+        Examples:
+            myeml.delete_title()
+        """
+        try:
+            path = LOOKUPS['creator']['path']
+            parent = LOOKUPS['creator']['parent']
+            target = LOOKUPS['creator']['target']
+            
+            if self.interactive == True:
+                quiet = False
+            else:
+                quiet = True
+
+            self._delete_node(path=path, parent=parent, target=target, quiet=quiet)
+            
+        except:
+            print('error delete_creator()') 
 
     def show_overview(self, node_xpath:str=None, depth:int=0):
         """Pretty-print up to three levels of xml tags and text
@@ -175,7 +199,7 @@ class Eml():
             node (Node): The metapype.model.node.Node at which the overview should start. 
             depth (int): The depth to at which you want to start the overview. 0 starts the overview at the supplied `node_xpath`. 1 starts the overview at `node_xpath`'s children. -1 starts the overview at `node_xpath`'s parent. Defaults to 0.
         """
-        if depth < 2:
+        if depth < 3:
             spaces = '    ' * depth
             if depth == 0:
                 print(f'{spaces}<{node.name}>')
@@ -190,19 +214,65 @@ class Eml():
     def _set_node():
         pass
 
-    def _delete_node():
-        pass
+    def _delete_node(self, path:list, parent:list, target:str, quiet:bool):
+        try:
+            # find any existing nodes
+            values = self.eml.find_all_nodes_by_path(path=path)
 
+            if values is None or len(values) == 0: # if there's no node, raise exception
+                raise MissingNodeException(target)
+            else: # if there's a node, work through possible parent node cases
+                parent = self.eml.find_all_nodes_by_path(path=parent) # collect parent nodes
+
+                # case 1: there are many parent nodes
+                # raise exception because our algo can't know which parent node to append to; this must be resolved via the LOOKUPS dict
+                # should be impossible except when invalid xml exists or the LOOKUPS dict is wrong
+                assert len(parent) < 2, print(f'{bcolors.FAIL + bcolors.BOLD + bcolors.UNDERLINE}Process execution failed.\n{bcolors.ENDC}\nReturned multiple parent{bcolors.BOLD}`node`{bcolors.ENDC}s.\n Invalid EML xml causes this error.\nUse `myeml.validate()` to determine why your EML is invalid and for suggested fixes.')
+                # case 2: there are no parent nodes
+                # should be impossible except when invalid xml exists
+                assert len(parent) > 0, print(f'{bcolors.FAIL + bcolors.BOLD + bcolors.UNDERLINE}Process execution failed.\n{bcolors.ENDC}\nReturned zero parent{bcolors.BOLD}`node`{bcolors.ENDC}s.\n Invalid EML xml causes this error.\nUse `myeml.validate()` to determine why your EML is invalid and how to fix it.')
+                # case 2: there's one parent node so we delete children (this is our base-case)
+                # if quiet is True, silently delete nodes
+                # else, ask permission to delete nodes
+                if len(parent) == 1:
+                    parent = parent[0]
+                    if quiet == True:
+                        for value in values:
+                            parent.remove_child(child=value)
+                            self.eml.delete_node_instance(id=value.id)
+                    else:
+                        if len(values) == 1:
+                            print(f'{bcolors.WARNING + bcolors.BOLD + bcolors.UNDERLINE}Warning!{bcolors.ENDC}\nMetadata package contains one `{bcolors.BOLD}{target}{bcolors.ENDC}` node:')
+                        if len(values) > 1:
+                            print(f'{bcolors.WARNING + bcolors.BOLD + bcolors.UNDERLINE}Warning!{bcolors.ENDC}\nMetadata package contains {len(values)} `{bcolors.BOLD}{target}{bcolors.ENDC}` nodes:')
+                        self._get_node(path=path, target=target, pretty=True, quiet=False)
+                        overwrite = input(f'{bcolors.BOLD}Do you want to delete these node(s)?\n{bcolors.ENDC}("{bcolors.BOLD}y{bcolors.ENDC}" to delete, "{bcolors.BOLD}n{bcolors.ENDC}" to cancel.)\n\n')
+                        print(f'User input: {overwrite}')
+                        if overwrite.lower() == 'y':
+                            for value in values:
+                                parent.remove_child(child=value)
+                                self.eml.delete_node_instance(id=value.id)
+                            print(f'`{bcolors.BOLD}{target}{bcolors.ENDC}` deleted.')
+                        else:
+                            print(f'`{bcolors.BOLD}{target}{bcolors.ENDC}` deletion cancelled.')
+                
+                # algo needs to build parent nodes
+                elif len(parent) == 0:
+                    pass
+
+        except MissingNodeException as e:
+            if quiet == False:
+                print(e.msg)
+
+    def validate(self):
+        """Validate that an EML object will render to valid EML xml
+
+        This method should rely on:
+        emlvp.validator.Validator
+        """
+        pass
+    
     def _get_node(self, path:list, target:str, pretty:bool, quiet:bool):
-        # nodes = self.eml.find_all_nodes_by_path(path=path)
-        # target_ids = []
-        # for target in nodes:
-        #     target_ids.append(target.id)
-        # target_nodes = []
-        # for target in target_ids:
-        #     target_nodes.append(self.eml.get_node_instance(target))
-        # for target in target_nodes:
-        #     self.show_overview(target)
 
         try:
             # collect all nodes at `path`
